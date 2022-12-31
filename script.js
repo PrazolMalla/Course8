e8e4683ee83726f0f63f87567d5f363f40bf007f
Prazol Malla <prajwalmalla16@gmail.com>
2022-12-31 12:27:15 +0545

    challenge 1

--- script.js
+++ script.js
@@ -253,3 +253,75 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 */
+
+//? Challenge 1
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+  printGoal: function (...goalScorer) {
+    for (let i = 0; i < goalScorer.length; i++) {
+      console.log(goalScorer[i]);
+    }
+    console.log(goalScorer.length);
+  },
+};
+
+const [player1, player2] = game.players;
+console.log(player1, player2);
+
+const [gk, ...fieldPlayers] = player1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+game.printGoal('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+game.printGoal(...game.scored);
+
+// const win = team1 < team2 && team2 < team1;
+// console.log(win);
+team1 < team2 && console.log('team1 is more likely to win');
+team2 < team1 && console.log('team2 is more likely to win');
