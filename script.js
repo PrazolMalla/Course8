71c4809a08322d51851a0b2012564d21ae6be430
Prazol Malla <prajwalmalla16@gmail.com>
2023-01-07 20:07:14 +0545

    Coding challege 3 completed

--- script.js
+++ script.js
@@ -52,7 +52,37 @@ const restaurant = {
   },
 };
 
-//?
+//? coding challenge 3
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}:${event}`);
+}
+
+//? maps
 
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
